f8427fa0e1d803154eedc8ccf3f6b5d5eda48e29
Gavin-Main <gavinroberts419@gmail.com>
2025-09-29 14:52:11 -0700

    Ensuring saved changes.

--- src/geolocate.py
+++ src/geolocate.py
@@ -1,6 +1,6 @@
 import geoip2.database
 
-def geolocate_ip(ip, db_path="../geoip2-db/GeoLite2-City.mmdb"):
+def geolocate_ip(ip, db_path="../geoip2-db/GeoLite2-ASN.mmdb"):
     try:
         reader = geoip2.database.Reader(db_path)
         response = reader.city(ip)
